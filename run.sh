74ad21fc0148f18972215fa99feb67f296fdb52c
Cary hu <cary.hu@grapecity.com>
2022-06-13 21:51:02 +0800

    feat: update server port

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ docker build . -t zhongtingicu
 
 docker stop zhongtingicu-container
 docker rm zhongtingicu-container
-docker run -d --name zhongtingicu-container -p 8521:80 zhongtingicu
+docker run -d --name zhongtingicu-container -p 8621:80 zhongtingicu
